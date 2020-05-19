0514bd13f2c2963e293c3f16b21eb37f6413af77
Patrick Lybarger <lybargerp2000@yahoo.com>
2020-05-19 14:31:15 -0600

    Ensuring APIKEY function is ignored when commiting.

---
+++ src/components/APIKEY.js
@@ -0,0 +1,13 @@
+
+import { useState } from "react";
+
+const APIKEY = (props) => {
+    const [value, setValue] = useState(0);
+    return(
+              setValue(AIzaS)
+        )
+
+
+    
+}
+export default APIKEY;
--- src/components/Container.js
+++ src/components/Container.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import APIKEY from './APIKEY';
 
 const Container = (props) => {
     const [collections, setCollections] = useState({});
 
     useEffect(() => {
-        axios.get('https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=APIKEY=snippet,contentDetails,statistics,status')
+        axios.get(`https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=${APIKEY.useState}=snippet,contentDetails,statistics,status`)
         .then(response => setCollections(response.data[props.index])
         
         )}, [collections, props.index]);
